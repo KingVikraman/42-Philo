c1ffed34e591ed444ea5a686389eb3090b971f28
Raja Vikraman <rajavikraman2006@gmail.com>
2025-03-23 21:24:05 +0800

    Commit message for 22/03/2025 -> i modified the logic of the parsing_2.c.
    Well for today i actually did something quite intresting, i realised that when
    i was doing my init_all once completing my init_mutex and init_philo, i realised that
    i have to once again change the values parsed in my arguments into integers.
    
    I initially thought that once i change it in my is_valid_check i dont have to bother about
    that but i guess i was wrong, thus i came up with an idea where what if in the is_Valid_check
    itself i change the data type and store it straing into a struct, thus i can just do atoi once
    and use it throughout the function later.
    
    So that what i have been trying. Cracking  my head and makeing  ends meet. There were plenty of
    errors cause the use of struct were not proper thus i had to make it till i win it.

--- Philo/philo/source/parsing_2.c
+++ Philo/philo/source/parsing_2.c
@@ -1,4 +1,18 @@
 #include "../includes/philo.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "pthread.h"
+
+
+typedef struct s_args
+{
+    int total_philo_num;
+    int death_time;
+    int eating_time;
+    int sleeping_time;
+    int total_meals;
+} t_args;
 
 
 
@@ -26,35 +40,41 @@ static int ft_atoi(char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		nb = nb * 10 + (str[i] - '0');
-		if (nb > INT_MAX)  // Handle overflow
+		if (nb > INT_MAX)  // Handle overflow 
 			return -1;
 		i++;
 	}
 	return (int)nb;
 }
 
-bool is_valid_check(char **argv)
+
+bool is_valid_check(char **argv, t_args *args)
 {
-	int i = 0;
-	int nb;
+    int i = 0;
+    int nb;
 
- 	while (argv[++i])
-	{
-		if (!ft_isdigit(argv[i]))
-			return (printf("Not a number: %s\n", argv[i]), false);
+    while (argv[++i])
+    {
+        if (!ft_isdigit(argv[i]))
+            return (printf("Not a number: %s\n", argv[i]), false);
 
-		//printf("input>> %s\n", argv[i]);
-		nb = ft_atoi(argv[i]);
-		//printf("output>> %d\n", nb);
+        nb = ft_atoi(argv[i]);
 
-		if (i == 1 && (nb <= 0 || nb > MAX_PHILOS))
-			return (printf("Invalid number of philos: %d\n", nb), false);
+        if (i == 1 && (nb <= 0 || nb > MAX_PHILOS))
+            return (printf("Invalid number of philos: %d\n", nb), false);
 
-		if (i != 1 && nb == -1)
-			return (printf("Number is a negative value or overflowed: %d\n", nb), false);
+        if (i != 1 && nb == -1)
+            return (printf("Number is a negative value or overflowed: %d\n", nb), false);
 
-	}
-	return true;
+        //Stores the converted values inside the struct
+        if (i == 1) args->total_philo_num = nb;
+        if (i == 2) args->death_time = nb;
+        if (i == 3) args->eating_time = nb;
+        if (i == 4) args->sleeping_time = nb;
+        if (i == 5) args->total_meals = nb;
+    }
+
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -65,3 +85,36 @@ int main(int argc, char **argv)
 		return (1);
 	printf("OK\n");
 }
+
+// bool is_valid_check(char **argv)
+// {
+// 	int i = 0;
+// 	int nb;
+
+//  	while (argv[++i])
+// 	{
+// 		if (!ft_isdigit(argv[i]))
+// 			return (printf("Not a number: %s\n", argv[i]), false);
+
+// 		//printf("input>> %s\n", argv[i]);
+// 		nb = ft_atoi(argv[i]);
+// 		//printf("output>> %d\n", nb);
+
+// 		if (i == 1 && (nb <= 0 || nb > MAX_PHILOS))
+// 			return (printf("Invalid number of philos: %d\n", nb), false);
+
+// 		if (i != 1 && nb == -1)
+// 			return (printf("Number is a negative value or overflowed: %d\n", nb), false);
+
+// 	}
+// 	return true;
+// }
+
+// int main(int argc, char **argv)
+// {
+// 	if (argc != 5 && argc != 6)
+// 		return(printf("%s", STR_ERR_VALID), 1);
+// 	if (!is_valid_check(argv))
+// 		return (1);
+// 	printf("OK\n");
+// }
---
+++ Philo/philo/source/utils.c
@@ -0,0 +1,100 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "pthread.h"
+#include "stdbool.h"
+#include "limits.h"
+
+#define MAX_PHILOS 200  // Define MAX_PHILOS
+
+typedef struct s_args
+{
+    int total_philo_num;
+    int death_time;
+    int eating_time;
+    int sleeping_time;
+    int total_meals;
+} t_args;
+
+static bool	ft_isdigit(char	*str)
+{
+	int i = 0;
+
+	if (!str[i])  // Handle empty strings
+		return false;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return false;
+		i++;
+	}
+	return true;
+}
+
+static int ft_atoi(char *str)
+{
+	int i = 0;
+	unsigned long nb = 0;
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		if (nb > INT_MAX)  // Handle overflow 
+			return -1;
+		i++;
+	}
+	return (int)nb;
+}
+
+bool is_valid_check(char **argv, t_args *args)
+{
+    int i = 0;
+    int nb;
+
+    while (argv[++i])
+    {
+        if (!ft_isdigit(argv[i]))
+            return (printf("Not a number: %s\n", argv[i]), false);
+
+        nb = ft_atoi(argv[i]);
+
+        if (i == 1 && (nb <= 0 || nb > MAX_PHILOS))
+            return (printf("Invalid number of philos: %d\n", nb), false);
+
+        if (i != 1 && nb == -1)
+            return (printf("Number is a negative value or overflowed: %d\n", nb), false);
+
+        //Stores the converted values inside the struct
+        if (i == 1) args->total_philo_num = nb;
+        if (i == 2) args->death_time = nb;
+        if (i == 3) args->eating_time = nb;
+        if (i == 4) args->sleeping_time = nb;
+        if (i == 5) args->total_meals = nb;
+    }
+
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+	t_args args;  // Declare args struct
+
+	if (argc != 5 && argc != 6)
+		return (printf("Error 404: Invalid arguments!\n"), 1);
+	
+	if (!is_valid_check(argv, &args))  // Pass args properly
+		return (1);
+
+	printf("🧵 Philosopher Simulation Settings:\n");
+	printf("Total Philosophers: %d\n", args.total_philo_num);
+	printf("Time to Die: %d ms\n", args.death_time);
+	printf("Time to Eat: %d ms\n", args.eating_time);
+	printf("Time to Sleep: %d ms\n", args.sleeping_time);
+	if (argc == 6)
+		printf("Total Meals: %d\n", args.total_meals);
+	else
+		printf("Total Meals: Unlimited\n");
+
+	return (0);
+}
