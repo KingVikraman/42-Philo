ed016aa62063f2f4f4416a9988f3e83b7e32a9d4
Raja Vikraman <rajavikraman2006@gmail.com>
2025-03-24 12:43:45 +0800

    Git commit for 22/03/2025 & 23/03/2025.
     For this commit, i was just trying to make the init_mutexes, and init_philo file
     more readable, and made it a litle more nicer to see, changed the structs a little,
    attempthed the init_all, to make the initialising process. Trying to understand how
    will the structs work in harmony with me using structs for the atoi.

---
+++ Philo/philo/initialising/init_all.c
@@ -0,0 +1,18 @@
+#include "stdlib.h"
+#include "unistd.h"
+#include "stdio.h"
+#include "pthread.h"
+
+
+typedef struct s_philo
+{
+	int 	sum_philo;
+	int 	feast_time;
+	int 	sleep_time;
+	int 	death_time;
+} t_philo;
+void	init_all(t_philo *philo, char **argv, int argc)
+{
+	data->sum_philo = (argv[1]);
+	data->death_time = ()
+}
