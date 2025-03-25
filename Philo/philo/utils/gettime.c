9a7ef9b0ee67700b9c28d485f84dda3fd27f1f95
Raja Vikraman <rajavikraman2006@gmail.com>
2025-03-25 20:54:33 +0800

    Git commit[ Update and learning(gettime.c)]...25/03/2025
    Git commit Updates:
    	-> Made a struct just to store time in seconds and miliseconds.
    	-> Used a utility function from the header <sys/time.h>, for
    	   gettimeofday.
    	-> Made a new logic in converting microseconds and seconds to
    	   Miliseconds.
    	-> This function returns the current time in milliseconds since
    	   January 1, 1970.
    
    Learning Info:
    	-> I learned about the sys/time library and what are the things
    	   that are usefull.
    	-> Learned about the time diffrence, like how the miliseconds and
    	   and seconds are stored, and the date the microsecond stared
    	    counting.
    	-> Learned a new logic and incorporated it into my code, the
    	   way the time.tv_sec and usec converts the time to miliseconds.
    	-> Learned about other usefull way to use this function in
    	   measuring execution time, logging and preformance monitoring.

--- Philo/philo/utils/gettime.c
+++ Philo/philo/utils/gettime.c
@@ -1,11 +1,30 @@
 long long int	gettime(void)
 {
-	struct timeval	time;
-	long long int	milisec_time;
+	struct timeval	time; // struct to store time in seconds and milisecods.
+	long long int	milisec_time; //Variable stores time in milisecods
 
-	gettimeofday(&time, NULL);
-	milisec_time = time.tv_sec * 1000 + time.tv_usec / 1000;
-	return (milisec_time);
+	gettimeofday(&time,NULL);// gets the current time of the day.
+	milisec_time = time.tv_sec * 1000 + time.tv_usec / 1000; // converts current time to miliseconds
+	return (milisec_time); // Returns time in miliseconds.
 }
 
-hello
+// struct timeval{
+// 	time_t		tv_sec; // Seconds since 1970,
+// 	suseconds	tv_usec; //(Microseconds, 1,000,000, microseconds = 1 second)
+// };
+
+// time.tv_sec -> Holds for seconds.
+// time.tv_sec -> Holds for miliseconds.
+
+// gettimeofday(&time, NULL);
+// ->system call retrieves the current time and stores it in time.
+//   The NULL argument is usually used for time zone info.[Lowkey i think NULL might not be nessasary].
+
+// milisec_time = time.tv_sec * 1000 + time.tv_usec / 1000;
+// ->time.tv_sec * 1000 converts seconds to milliseconds.
+// ->time.tv_usec / 1000 converts microseconds to milliseconds (since 1,000,000 µs = 1 sec).
+
+// TL;DR
+// 👉 This function returns the current time in milliseconds since January 1, 1970.
+// 👉 It uses gettimeofday() to get seconds and microseconds, then converts it to milliseconds.
+// 👉 Useful for measuring execution time, logging, and performance monitoring.
