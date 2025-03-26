fa357348c63e3b3bf77fbaa7bcce99b06f94928b
Raja Vikraman <rajavikraman2006@gmail.com>
2025-03-26 20:07:54 +0800

    Added Header file to Git, [Update]-> 26/03/2025..
    Modified my philo.h, added the other structs and
    a few other macros for my code.

--- Philo/philo/includes/philo.h
+++ Philo/philo/includes/philo.h
@@ -10,7 +10,29 @@
 #define STD_ERR_NUMBERS	 "Arguments must be positive numbers!"
 #define STD_ERR_LIMITS	 "One or more arguments are out of range!"
 #define	STD_ERR_START	"All arguments are valid! Starting the simulation..."
+#define ERR_00 "Starting mutex initialisation\n"
+#define ERR_01 "Initialising the mutex for the fork %d.\n"
+#define ERR_02 "FAILURE:Fork also cannot initialise, HAIYA!!!.\n"
+#define ERR_03 "The forks ahh is connected ahh successfullyy!!.\n"
+#define ERR_04 "Initialising the writing lock ahh!\n "
+#define ERR_05 "FAILURE: Writing lock also cannot initialise. GG!!!\n."
+#define ERR_06 "The writing locks ahh is connected ahh successfully!!\n"
+#define ERR_07 "All mutexes are initialised smoothly!\n"
 
+
+typedef struct s_data{
+	int philo_sum;
+	pthread_mutex_t *fork;
+	pthread_mutex_t writing_lock;
+	//t_args args;
+} t_data;
+typedef struct s_args{
+	int philo_sum;
+	int death_time;
+	int feasting_time;
+	int sleeping_time;
+	int meals_sum;
+} t_args;
 
 
 // Example of a struct:
@@ -57,3 +79,16 @@ the Race condition,
 
 
 Lone Philo routine*/
+
+
+int hello(int *x)
+{
+	*x = 10;
+}
+int main()
+{
+	int x;
+	hello(&x);
+	printf("%d\n", x);
+}
+
